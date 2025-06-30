bf11128758c648f5ab4bc85d0615e341fa319263
Gallind <gal.lind@post.runi.ac.il>
2025-06-30 17:42:01 +0300

    check3

--- RTS_test1/Assets/Scripts/UnitIdleState.cs
+++ RTS_test1/Assets/Scripts/UnitIdleState.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 
+
 public class UnitIdleState : StateMachineBehaviour
 {
     AttackController attackController;
@@ -16,13 +17,13 @@ public class UnitIdleState : StateMachineBehaviour
         if (attackController.targetToAttack != null)
         {
             animator.SetBool("isFollow", true);
-       }
+        }
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     // override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     // {
-       
+
     // }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
