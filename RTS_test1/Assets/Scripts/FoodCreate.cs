5ad5dc5a15755abad184b64c5df6178032c07213
guyro85 <guynevo8@gmail.com>
2025-06-30 18:54:42 +0300

    edited terrain

--- RTS_test1/Assets/Scripts/FoodCreate.cs
+++ RTS_test1/Assets/Scripts/FoodCreate.cs
@@ -5,7 +5,7 @@ public class FoodCreate : MonoBehaviour
 {
     private void OnTriggerStay(Collider other)
     {
-        if (other.CompareTag("Hero"))
+        if (other.CompareTag("Hero") && !other.transform.Find("Food").gameObject.activeSelf)
         {
             float dist = Vector3.Distance(transform.position, other.transform.position);
             if (dist < 1.6f)
